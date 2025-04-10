557e20166b36dce7a13c085f5a3eda71cd3b3e35
vpktirumalluri <vpktirumalluri@compreo.ai>
2025-04-10 19:35:43 +0530

    Add exception handling middleware and error response model; implement validation for CreateProductDTO and add BuggyController for testing API responses

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,40 @@
+using System;
+using API.DTOs;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController: BaseApiController
+{
+
+    [HttpGet("notfound")]
+    public IActionResult GetNotFoundRequest()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("internalerror")]
+    public IActionResult GetInternalError()
+    {
+        throw new Exception("This is a test error");
+    }
+
+    [HttpPost("validationerror")]
+    public IActionResult GetValidationError(CreateProductDTO product)
+    {
+        return Ok();
+    }
+
+    [HttpGet("bad-request")]
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("Not a good request");
+    }
+
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+}
---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Net;
+using System.Text.Json;
+using API.Errors;
+using Microsoft.Extensions.Hosting;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware(IHostEnvironment env, RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex, env);
+        }
+    }
+    private static Task HandleExceptionAsync(HttpContext context, Exception ex, IHostEnvironment env)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var response = new ApiErrorResponse(context.Response.StatusCode, ex.Message, env.IsDevelopment() ? ex.StackTrace : "Internal Server Error");
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        var json = JsonSerializer.Serialize(response, options);
+
+        return context.Response.WriteAsJsonAsync(json);
+    }
+}
