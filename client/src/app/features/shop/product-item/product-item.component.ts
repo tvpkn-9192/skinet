2d208bcd414633dd0e035ed07f6003af26d83c96
vpktirumalluri <vpktirumalluri@compreo.ai>
2025-04-14 23:03:35 +0530

    Implement shop feature with product filtering, pagination, and search functionality

---
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, input } from '@angular/core';
+import { Product } from '../../../shared/models/product';
+import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card';
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-product-item',
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
+    MatIcon,
+    CurrencyPipe
+  ],
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.scss'
+})
+export class ProductItemComponent { 
+  @Input() product?: Product;
+}
---
+++ client/src/app/features/shop/shop.component.ts
@@ -0,0 +1,100 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ShopService } from '../../core/services/shop.service';
+import { Product } from '../../shared/models/product';
+import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatDialog } from '@angular/material/dialog';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { ShopParams } from '../../shared/models/shopParams';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { Pagination } from '../../shared/models/pagination';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-shop',
+  imports: [
+    ProductItemComponent,
+    MatButton,
+    MatIcon,
+    MatMenu,
+    MatSelectionList,
+    MatListOption,
+    MatMenuTrigger,
+    MatPaginator,
+    FormsModule,
+  ],
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.scss'
+})
+export class ShopComponent implements OnInit {
+  private shopService = inject(ShopService);
+  private dialogService = inject(MatDialog);
+  products?: Pagination<Product>;
+  shopParams = new ShopParams();
+  pageSizeOptions = [5, 10, 15, 20];
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low-High', value: 'priceAsc'},
+    {name: 'Price: High-Low', value: 'priceDesc'},
+  ]
+
+  ngOnInit() {
+    this.initializeShop();
+  }
+
+  initializeShop() {
+    this.shopService.getTypes();
+    this.shopService.getBrands();
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: (response) => this.products = response,
+      error: (error) => console.error(error)
+    });
+  }
+
+  onSearchChange() {
+    this.shopParams.pageNumber = 1; // Reset to first page on search change
+    this.getProducts();
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.shopParams.pageNumber = event.pageIndex + 1; 
+    this.shopParams.pageSize = event.pageSize;
+    this.getProducts();
+  }
+
+  onSortChange(event: MatSelectionListChange) {
+    const selectedOption = event.options[0];
+    if (selectedOption) {
+      this.shopParams.sort = selectedOption.value;
+      this.shopParams.pageNumber = 1; // Reset to first page on sort change
+      this.getProducts();
+    }
+  }
+
+  openFiltersDialog() {
+    const dialogRef = this.dialogService.open(FiltersDialogComponent, {
+      width: '500px',
+      data: {
+        selectedBrands: this.shopParams.brands,
+        selectedTypes: this.shopParams.types,
+      }
+    });
+    dialogRef.afterClosed().subscribe({
+      next: result => {
+        if (result) {
+          this.shopParams.brands = result.selectedBrands;
+          this.shopParams.types = result.selectedTypes;
+          this.shopParams.pageNumber = 1; // Reset to first page on filter change
+          this.getProducts();
+        }
+      }
+    });
+  }
+}
