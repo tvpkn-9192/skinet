8f5f0fccf9547697efc1668c254cdfbb8622152b
vpktirumalluri <vpktirumalluri@compreo.ai>
2025-04-15 00:15:00 +0530

    Add routing and product details functionality

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -24,6 +24,10 @@ export class ShopService {
     return this.http.get<Pagination<Product>>(this.baseUrl + 'products', {params})
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getTypes() {
     if (this.types.length > 0) return this.types;
     return this.http.get<string[]>(this.baseUrl + 'products/types').subscribe({
---
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ShopService } from '../../../core/services/shop.service';
+import { Product } from '../../../shared/models/product';
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatDivider } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-product-details',
+  imports: [
+    CurrencyPipe,
+    MatButton,
+    MatIcon,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatDivider
+  ],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.scss'
+})
+export class ProductDetailsComponent implements OnInit {
+  private shopService = inject(ShopService);
+  private activatedRoutes = inject(ActivatedRoute);
+  product?: Product;
+
+  ngOnInit() {
+    this.loadProduct();
+  }
+  loadProduct() { 
+    const id = this.activatedRoutes.snapshot.paramMap.get('id');
+    if(!id) return;
+    this.shopService.getProduct(+id).subscribe({
+      next: (product) => this.product = product,
+      error: (error) => console.log(error)
+    });
+  }
+}
