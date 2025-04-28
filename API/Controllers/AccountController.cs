88f795429806aedd9ab212e67bd8db22737b4b62
vpktirumalluri <vpktirumalluri@compreo.ai>
2025-04-28 23:35:10 +0530

    Add user registration

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Security.Claims;
+using API.DTOs;
+using API.Extensions;
+using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(SignInManager<AppUser> signInManager): BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult> Register(RegisterDTO registerDTO)
+    {
+        //if (registerDTO == null) return BadRequest("Invalid registration data.");
+
+        var user = new AppUser
+        {
+            FirstName = registerDTO.FirstName,
+            LastName = registerDTO.LastName,
+            Email = registerDTO.Email,
+            UserName = registerDTO.Email
+        };
+
+        var result = await signInManager.UserManager.CreateAsync(user, registerDTO.Password);
+         if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem();
+        }
+
+        return Ok();
+    }
+
+    [Authorize]
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+        return NoContent();
+    }
+
+    [HttpGet("user-info")]
+    public async Task<ActionResult> GetUserInfo()
+    {
+        if (User.Identity?.IsAuthenticated == false) return NoContent();
+
+        var user = await signInManager.UserManager.GetUserByEmailWithAddress(User);
+        return Ok(new
+        {
+            user.FirstName,
+            user.LastName,
+            user.Email,
+            Address = user.Address?.ToDto()
+        });
+    }
+
+    [HttpGet]
+    public ActionResult GetAuthState() 
+    {
+        return Ok(new { isAuthenticated = User.Identity?.IsAuthenticated ?? false });
+    }
+
+     [Authorize]
+    [HttpPost("address")]
+    public async Task<ActionResult<Address>> CreateOrUpdateAddress(AddressDto addressDto)
+    {
+        var user = await signInManager.UserManager.GetUserByEmailWithAddress(User);
+
+        if (user.Address == null)
+        {
+            user.Address = addressDto.ToEntity();
+        }
+        else 
+        {
+            user.Address.UpdateFromDto(addressDto);
+        }
+
+        var result = await signInManager.UserManager.UpdateAsync(user);
+
+        if (!result.Succeeded) return BadRequest("Problem updating user address");
+
+        return Ok(user.Address.ToDto());
+    }
+}
--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Security.Claims;
 using API.DTOs;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -36,5 +38,14 @@ public class BuggyController: BaseApiController
     public IActionResult GetUnauthorized()
     {
         return Unauthorized();
+    }
+
+    [Authorize]
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+        var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; 
+        return Ok("Hello " + name + ", your id is: " + id);  
     }
 }
